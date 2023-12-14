dd0b7c1b206059741aa0a2d2fa4f4bbb6b8ba9ed
santirinaldi <santiagorinaldi97@gmail.com>
2023-12-14 12:46:23 -0300

    adj

--- components/Sections/Album/Album.js
+++ components/Sections/Album/Album.js
@@ -2,6 +2,8 @@ import styles from "./styles.module.scss";
 
 const Album = (props) => {
 
+    console.log("test");
+
     return (
         <div className={styles.album}>
             <p>{props.client}</p>
